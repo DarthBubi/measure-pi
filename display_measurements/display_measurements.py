b9237d4a55f8252d7fe86594435a8448cc0f8692
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2017-11-09 18:09:37 +0000

    Generate valid image for the eink display

--- display_measurements/display_measurements.py
+++ display_measurements/display_measurements.py
@@ -1,66 +1,44 @@
 #!/usr/bin/python
 # coding=utf-8
 
+import sys
+sys.path.append("../measure")
+from quantiser import Quantiser
+from read_sensor_data import read_temp
 import cv2
 import numpy as np
-from quantiser import Quantiser
 
-# class Quantiser(object):
-#     def __init__(self, num_colour=4):
-#         self.num_colour = num_colour
-#         self.divisor = 256 / num_colour
-#         self.max_quantised_value = 255 / self.divisor
-
-#     def quantise_pixel(self, val):
-#         return ((val / self.divisor) * 255) / self.max_quantised_value
-
-#     def quantise_image(self, image):
-#         quantised_image = image.copy()
-#         it = np.nditer(quantised_image,
-#                        flags=['external_loop', 'buffered'],
-#                        op_flags=['readwrite'],
-#                        casting='safe')
-
-#         for p in it:
-#             p[...] = map(self.quantise_pixel, p)
-
-#         return quantised_image
+def scan_right_to_left(img):
+    it = np.nditer(img, flags=['external_loop'], op_flags=['readwrite'])
+    for p in img:
+        p[...] = p[::-1]
 
 
 def write_measurements_to_image(quantiser, temp=None, humid=None):
-    img = np.full((600, 800, 1), 255, np.uint8)
-    font = cv2.FONT_HERSHEY_PLAIN
-    cv2.putText(img, 'HelloWorld', (5, 100), font, 3, (0), 2, cv2.CV_AA)
-    cv2.putText(img, 'FooBar', (5, 200), font, 3, (0), 2, cv2.CV_AA)
-    # img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = np.full((600, 800, 1), 0, np.uint8)
+    font = cv2.FONT_HERSHEY_DUPLEX
+    pixel_v = 50
 
-    # e1 = cv2.getTickCount()
-    # for r in range(img.shape[0]):
-    #     for c in range(img.shape[1]):
-    #         img_grey[r, c] = quantiser.quantise_pixel(img_grey[r, c])
+    if temp is not None:
+        for t in temp:
+            text = "Room temperature: " + str(t) + " Celsius"
+            cv2.putText(img, text, (5, pixel_v), font, 1.5, 255, 4, cv2.CV_AA)
+            pixel_v = pixel_v + 100
 
-    # e2 = cv2.getTickCount()
-    # dt = (e2 - e1)/cv2.getTickFrequency()
-    # print("Ellapsed time: " + str(dt))
-
-    # e1 = cv2.getTickCount()
-    # foo = np.array([map(quantiser.quantise_pixel, p) for p in img_grey])
-    # e2 = cv2.getTickCount()
-    # dt = (e2 - e1)/cv2.getTickFrequency()
-    # print("Ellapsed time: " + str(dt))
-
-    e1 = cv2.getTickCount()
     img_grey = quantiser.quantise_image(img)
-    e2 = cv2.getTickCount()
-    dt = (e2 - e1)/cv2.getTickFrequency()
-    print("Ellapsed time: " + str(dt))
+    scan_right_to_left(img_grey)
+    # Debug image
+    # cv2.imwrite("test_grey.bmp", img_grey)
 
-    cv2.imwrite("test.bmp", img)
-    cv2.imwrite("test_grey.bmp", img_grey)
+    with open("img.npy", 'w') as file:
+        img_grey = img_grey[:, 0::4].flatten()
+        # img_grey = img_grey.byteswap() # doesn't work for some reason
+        np.save(file, img_grey)
 
-    with open("img.txt", 'w') as file:
-        np.savetxt(file, img_grey, fmt='%X')
 
 if __name__ == '__main__':
+    sensor_id = "28-000009aeb3f5"
+    sensor_name = "/sys/bus/w1/devices/%s/w1_slave" % sensor_id
     q = Quantiser()
-    write_measurements_to_image(q)
+    temp = [read_temp(sensor_name)]
+    write_measurements_to_image(q, temp)
