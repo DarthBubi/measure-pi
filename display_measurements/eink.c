55e74da55749374a030c13b8dd4d33977a122561
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2017-11-10 15:32:02 +0000

    Add code to send the generated image to the eink display

---
+++ display_measurements/eink.c
@@ -0,0 +1,82 @@
+#include <bcm2835.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define STATUS_PIN RPI_BPLUS_GPIO_J8_22
+#define CS_PIN RPI_BPLUS_GPIO_J8_24
+#define MOSI_PIN RPI_BPLUS_GPIO_J8_19
+#define MISO_PIN RPI_BPLUS_GPIO_J8_21
+#define SCK_PIN RPI_BPLUS_GPIO_J8_23
+
+#define FRAME_END_LEN 11
+
+#define SpiExchangeByte(x) bcm2835_spi_transfer(x)
+#define SPI_MODE SPI_MODE1
+
+unsigned char *gImage_image;
+
+bool digitalRead(uint8_t pin)
+{
+  if (bcm2835_gpio_lev(pin) == 1)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void digitalWrite(uint8_t pin, bool level)
+{
+  if (level == true)
+  {
+    bcm2835_gpio_set(pin);
+  }
+  else
+  {
+    bcm2835_gpio_clr(pin);
+  }
+}
+
+void SendImage(size_t imgSize)
+{
+  bool DeviceStatus;
+  unsigned char data;
+  unsigned char cnt0;
+  unsigned int cnt1;
+  unsigned long time;
+  bcm2835_spi_chipSelect(BCM2835_SPI_CS_NONE);
+  DeviceStatus = digitalRead(STATUS_PIN);
+  bcm2835_gpio_fsel(CS_PIN, BCM2835_GPIO_FSEL_OUTP);
+  bcm2835_gpio_clr(CS_PIN);
+  SpiExchangeByte(4);
+
+  while (digitalRead(STATUS_PIN) == DeviceStatus);
+
+  DeviceStatus = !DeviceStatus;
+  for (cnt0 = 0; cnt0 < FRAME_END_LEN; cnt0++)
+  {
+    printf("Frame %d \n", cnt0);
+    for (cnt1 = 0; cnt1 < imgSize; cnt1++)
+    {
+      data = gImage_image[cnt1];
+      SpiExchangeByte(data);
+      time = 0;
+      while (digitalRead(STATUS_PIN) == DeviceStatus)
+      {
+        time++;
+        if (time > 10000000)
+        {
+          printf("Error\n");
+          return;
+        }
+      }
+      DeviceStatus = !DeviceStatus;
+    }
+  }
+  bcm2835_gpio_set(CS_PIN);
+  bcm2835_gpio_fsel(CS_PIN, BCM2835_GPIO_FSEL_ALT0);
+  bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+  bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);
+}
---
+++ display_measurements/main.cpp
@@ -0,0 +1,29 @@
+#include <cnpy.h>
+#include <iostream>
+#include "eink.c"
+
+int main(int argc, char **argv)
+{
+  //Load the genearted image
+  cnpy::NpyArray img = cnpy::npy_load("img.npy");
+  gImage_image = img.data<unsigned char>();
+
+  if (!bcm2835_init())
+    return 1;
+
+  bcm2835_gpio_fsel(RPI_BPLUS_GPIO_J8_22, BCM2835_GPIO_FSEL_INPT);
+
+  bcm2835_spi_begin();
+  bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
+  bcm2835_spi_setDataMode(BCM2835_SPI_MODE1);
+  bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_64);
+  bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+  bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);
+  bcm2835_gpio_set_pud(RPI_BPLUS_GPIO_J8_22, BCM2835_GPIO_PUD_UP);
+
+  std::cout << "Sending image to eink display..." << std::endl;
+  SendImage(img.shape[0]);
+  std::cout << "Sending complete." << std::endl;
+
+  return 0;
+}
