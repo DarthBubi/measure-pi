a2a7c3ab16b0fd30ad24c7615d92f6bc6617bae9
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2018-07-09 12:18:33 +0200

    Add option to call logging in sensor data reader

--- measure/read_sensor_data.py
+++ measure/read_sensor_data.py
@@ -2,8 +2,11 @@
 # coding=utf-8
 
 from __future__ import division
+import argparse
 import re
 import RPi.GPIO as GPIO
+from logger import CsvLogger
+from datetime import datetime
 
 SENSOR_ID = "28-000009aeb3f5"
 SENSOR_NAME = "/sys/bus/w1/devices/%s/w1_slave" % SENSOR_ID
@@ -28,9 +31,24 @@ def read_temp(path):
     return temp
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--log-dir', type=str, default=None)
+    args = parser.parse_args()
+    dict_args = vars(args)
+
+    return dict_args
+
+
 if __name__ == '__main__':
     GPIO.setwarnings(False)
     temperature = read_temp(SENSOR_NAME)
+    args = parse_args()
 
     if temperature is not None:
-        print("Room temperature is: " + str(temperature) + " °C")
+        if not args["log_dir"]:
+            print("Room temperature is: " + str(temperature) + " °C")
+        else:
+            logger = CsvLogger(args["log_dir"] + "{}.csv".format(datetime.now().strftime("%Y-%m-%d")))
+            values = dict(time=datetime.now().strftime("%Y-%m-%d-%H-%M"), room_temperature=temperature)
+            logger.log(values)
