408a66c09020c566d0dd4c6f1730152b2a9ec7d9
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2025-09-09 18:39:02 +0200

    Support more sensors locally This adds support for the DHT22 and the BME280. Also some minor refactoring and optimzation

--- measure/influxdb_logger.py
+++ measure/influxdb_logger.py
@@ -7,12 +7,13 @@ import logging
 import requests
 import socket
 import time
+import sys
 
 from datetime import datetime, timezone
 from influxdb import InfluxDBClient
 from typing import Optional
 
-from read_sensor_data import read_temp
+from read_sensor_data import read_ds18b20, read_bme280, read_dht22
 
 
 class InfluxDBLogger:
@@ -28,41 +29,61 @@ class InfluxDBLogger:
         self.sensor_type: str = sensor_type
         self.node: str = node
 
-    def construct_db_string(self, temperature: float):
+    def construct_db_string(self, temperature: float, humidity: float | None=None, pressure: float | None=None) -> list[dict]:
+        fields = {}
+        fields["temperature"] = temperature
+        
+        if humidity is not None:
+            fields["humidity"] = humidity
+        if pressure is not None:
+            fields["pressure"] = pressure
+
         jason_string = [
             {
-                "measurement": self.measurement,
-                "tags": {
-                    "device": self.device,
-                    "node": self.node,
-                    "sensor": self.sensor_type
-                },
-                "time": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
-                "fields": {
-                    "temperature": temperature
-                }
+            "measurement": self.measurement,
+            "tags": {
+                "device": self.device,
+                "node": self.node,
+                "sensor": self.sensor_type
+            },
+            "time": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            "fields": fields
             }
         ]
 
         return jason_string
 
-    def log_temperature(self, sensor_id):
-        temperature = read_temp(sensor_id)
-        if temperature:
-            data = self.construct_db_string(temperature)
-            try:
-                self.influx_client.write_points(data)
-            except influxdb.exceptions.InfluxDBServerError as err:
-                logging.error(f"InfluxDB server error: {err}")
-            except requests.exceptions.ConnectionError as err:
-                logging.error(f"Connection error: {err}")
-            except Exception as err:
-                logging.error(f"Unexpected error: {err}")
+    def log_temperature(self, sensor_type, id=None, bus=None, address=None, pin=None) -> None:
+        temperature = humidity = pressure = None
+
+        if sensor_type == "DS18B20" and id is not None:
+            temperature = read_ds18b20(id)
+        elif sensor_type == "DHT22" and pin is not None:
+            temperature, humidity = read_dht22(pin)
+        elif sensor_type == "BME280" and bus is not None and address is not None:
+            temperature, pressure, humidity = read_bme280(bus, address)
         else:
+            logging.error(f"Unknown sensor type: {sensor_type} or insufficient parameters")
+            return
+
+        if temperature is None:
             logging.error("Error while reading temperature")
+            return
+
+        data = self.construct_db_string(temperature, humidity, pressure)
+        
+        try:
+            self.influx_client.write_points(data)
+        except influxdb.exceptions.InfluxDBServerError as err:
+            logging.error(f"InfluxDB server error: {err}")
+        except requests.exceptions.ConnectionError as err:
+            logging.error(f"Connection error: {err}")
+        except Exception as err:
+            logging.error(f"Unexpected error: {err}")
+        
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='InfluxDB logger')
     parser.add_argument('--host', type=str, required=False,
                         default='localhost',
@@ -88,16 +109,34 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-
-    sensor_id = args.sensor_id
-    sensor_name = f"/sys/bus/w1/devices/{sensor_id}/w1_slave"
-    client = InfluxDBLogger(args.device_name, args.sensor_type, args.node,
-                            args.measurement, args.database, host=args.host, port=args.port)
-
     logging.basicConfig(format='%(levelname)s %(asctime)s: %(message)s', level=logging.INFO)
+    
+    sensor_type = args.sensor_type
+
+    if sensor_type == "DS18B20":
+        sensor_id = args.sensor_id
+        sensor_name = f"/sys/bus/w1/devices/{sensor_id}/w1_slave"
+    elif sensor_type == "DHT22":
+        sensor_id = args.sensor_id
+    elif sensor_type == "BME280":
+        sensor_id = args.sensor_id
+    else:
+        logging.error(f"Unknown sensor type: {args.sensor_type}")
+        sys.exit(1)
+
+    client = InfluxDBLogger(args.device_name, args.sensor_type, args.node,
+                                args.measurement, args.database, host=args.host, port=args.port)
+
     while True:
         start = time.time()
-        client.log_temperature(sensor_name)
+        if sensor_type == "DS18B20":
+            client.log_temperature(sensor_type=sensor_type, id=sensor_name)
+        elif sensor_type == "DHT22":
+            client.log_temperature(sensor_type=sensor_type, pin=int(sensor_id))
+        elif sensor_type == "BME280":
+            bus, address = map(int, sensor_id.split(","))
+            client.log_temperature(sensor_type=sensor_type, bus=bus, address=address)
+
         end = time.time()
         logging.debug(end - start)
         time.sleep(30)
--- measure/read_sensor_data.py
+++ measure/read_sensor_data.py
@@ -4,6 +4,9 @@
 import argparse
 import re
 import RPi.GPIO as GPIO
+import adafruit_dht
+
+from bme280 import bme280, bme280_i2c
 from logger import CsvLogger
 from datetime import datetime
 
@@ -11,7 +14,7 @@ SENSOR_ID = f"28-000009aeb3f5"
 SENSOR_NAME = f"/sys/bus/w1/devices/{SENSOR_ID}/w1_slave"
 
 
-def read_temp(path):
+def read_ds18b20(path: str) -> float | None:
     """
     Read the current temperature from the connected DS18B20 sensor
 
@@ -30,7 +33,52 @@ def read_temp(path):
     return temp
 
 
-def parse_args():
+def read_bme280(bus: int, address: int) -> tuple[float | None, float | None, float | None]:
+    """
+    Read the current temperature and humidity from the BME280 sensor
+
+    :param bus: the I2C bus number
+    :param address: the I2C address of the BME280 sensor
+    :return: a tuple of (temperature, pressure, humidity)
+    """
+    bme280_i2c.set_default_i2c_address(bus)
+    bme280_i2c.set_default_bus(address)
+    
+    bme280.setup()
+    
+    sensor_values: bme280.Data = bme280.read_all()
+
+    temperature = round(sensor_values.temperature, 2)
+    pressure = round(sensor_values.pressure, 2)
+    humidity = round(sensor_values.humidity, 2)
+
+    return temperature, pressure, humidity
+
+
+def read_dht22(pin: int) -> tuple[float, float]:
+    """
+    Read the current temperature and humidity from the DHT22 sensor
+
+    :param pin: the GPIO pin number
+    :return: a tuple of (temperature, humidity)
+    """
+    dht_device = adafruit_dht.DHT22(pin)
+
+    try:
+        humidity, temperature = dht_device.humidity, dht_device.temperature
+    except RuntimeError as e:
+        print(f"Error reading DHT22 sensor: {e}")
+        raise e
+
+    if humidity is None or temperature is None:
+        raise RuntimeError("Failed to read from DHT22 sensor")
+    
+    humidity = round(humidity, 2)
+    temperature = round(temperature, 2)
+
+    return temperature, humidity
+
+def parse_args() -> dict:
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--log-dir', type=str, default=None)
     args = parser.parse_args()
@@ -41,7 +89,7 @@ def parse_args():
 
 if __name__ == '__main__':
     GPIO.setwarnings(False)
-    temperature = read_temp(SENSOR_NAME)
+    temperature = read_ds18b20(SENSOR_NAME)
     args = parse_args()
 
     if temperature is not None:
